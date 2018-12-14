d251218bc10c7bf08f36d025446a51ffa89255ed
Rémy Bonté-Duval <remy.duval@hotmail.fr>
2018-12-14 14:23:35 +0100

    1rst commit

--- app/scripts/SabineExp.js
+++ app/scripts/SabineExp.js
@@ -34,30 +34,28 @@ export default class App {
 
     constructor() {
 
-            //Stats
-            this.stats = new Stats();
-            this.stats.setMode(0); // 0: fps, 1: ms
-            this.stats.domElement.style.position = 'absolute';
-            this.stats.domElement.style.top = '0px';
-            this.stats.domElement.style.left = '0px';
-            document.body.appendChild( this.stats.domElement );
+        //Stats
+        this.stats = new Stats();
+        this.stats.setMode(0); // 0: fps, 1: ms
+        this.stats.domElement.style.position = 'absolute';
+        this.stats.domElement.style.top = '0px';
+        this.stats.domElement.style.left = '0px';
+        document.body.appendChild( this.stats.domElement );
 
-            //Gui
-            const gui = new dat.GUI();
+        //Gui
+        const gui = new dat.GUI();
 
+        // Sound
         this.play = new LoadSound();
-
 
         //THREE SCENE
         this.container = document.querySelector( '#main' );
     	document.body.appendChild( this.container );
 
-        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.001, 10 );
+        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10000 );
 
-        this.camera.position.x = 0.3;
-        this.camera.position.y = 0.3;
-        this.camera.position.z = Math.pow(1.7, -17);
 
+        this.camera.position.z = 50;
         this.controls = new OrbitControls(this.camera)
 
         this.scene = new THREE.Scene();
@@ -66,15 +64,15 @@ export default class App {
         var loader = new THREE.OBJLoader();
         loader.load(objFile,
             ( modelObj )=> {
-                //modelObj.scale.set(0.0001,0.0001,0.0001)
-                modelObj.scale.set(0.0005,0.0005,0.0005)
-                var lambert = new THREE.MeshLambertMaterial({color: 0xffffff});
+                modelObj.traverse( function (child) {
+                    if (child instanceof THREE.Mesh) {
+                        child.material = new THREE.MeshLambertMaterial({color: 0xfafbfc});
+                        child.castShadow = true; //default is false
+                    }
+                })
+                modelObj.scale.set(0.01,0.01,0.01)
+                modelObj.position.y = +0.5;
                 this.scene.add( modelObj );
-                modelObj.position.y = -0.1;
-                modelObj.material = lambert;
-                console.log(modelObj)
-                //modelObj.material = new THREE.MeshNormalMaterial()
-                //modelObj.material.shading = THREE.SmoothShading;
             },
             (xhr) => {
                 console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
@@ -86,32 +84,43 @@ export default class App {
 
 
         //LIGHT
-            //Directional
-            this.light = new THREE.DirectionalLight({color: 0x0000ff,intensity : 1})
-            this.light.position.x = -0.5
-            this.light.position.y = 0.8
-            this.scene.add(this.light)
+        //Hemisphere
+        this.hemiLight = new THREE.HemisphereLight( 0xffff00, 0x0000ff, 1 );
+        this.scene.add( this.hemiLight );
 
-        //TORUS
-        let geoTorus = new THREE.TorusGeometry( 0.09, 0.002, 30,80 );
-        let matTorus = new THREE.MeshNormalMaterial();
-        this.torus = new THREE.Mesh( geoTorus, matTorus );
-        this.scene.add(this.torus)
 
-        let geoTorusM = new THREE.TorusGeometry( 0.095, 0.003, 30,80 );
-        let matTorusM = new THREE.MeshNormalMaterial();
-        this.torusM = new THREE.Mesh( geoTorusM, matTorusM );
-        this.scene.add(this.torusM)
+        this.ambientLight = new THREE.AmbientLight( 0x0f0f0f, 1 );
+        // this.scene.add( this.ambientLight );
+        //Directional
+        this.dirLight = new THREE.DirectionalLight( 0x0000ff, 0.5 )
+        this.dirLight.castShadow = true
+        this.dirLight.shadowMapWidth = 1024; // default is 512
+        this.dirLight.shadowMapHeight = 1024; // default is 512  
+        this.dirLight.position.x = -1
+        this.dirLight.position.y = 8
+        this.dirLight.position.z = 6
+        this.scene.add(this.dirLight)
 
-        let geoTorusS = new THREE.TorusGeometry( 0.1, 0.004, 30,80 );
-        let matTorusS = new THREE.MeshNormalMaterial();
-        this.torusS = new THREE.Mesh( geoTorusS, matTorusS );
-        this.scene.add(this.torusS)
+        this.dirLightHelper = new THREE.DirectionalLightHelper( this.dirLight, 10 );
+        this.scene.add( this.dirLightHelper );
+
+
+
+        //BACKGROUND
+        let bgSize = 50;
+        let backgroundGeo = new THREE.BoxGeometry( bgSize, bgSize, bgSize );
+        let backgroundMat = new THREE.MeshLambertMaterial( { color: 0xfcfbfa, side: THREE.DoubleSide, shadowSide: THREE.DoubleSide } );
+        this.backgroundMesh = new THREE.Mesh( backgroundGeo, backgroundMat );
+        this.backgroundMesh.receiveShadow = true; //default is false
+        this.backgroundMesh.position.y = -bgSize*0.5;
+        this.scene.add( this.backgroundMesh );
 
 
         //RENDERER
     	this.renderer = new THREE.WebGLRenderer( { antialias: true, alpha:true } );
         this.renderer.setClearColor( '#eee' )
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
         this.renderer.setPixelRatio( window.devicePixelRatio );
     	this.renderer.setSize( window.innerWidth, window.innerHeight );
     	this.container.appendChild( this.renderer.domElement );
@@ -126,20 +135,6 @@ export default class App {
     render(bloomPass) {
         this.stats.begin();
         let time = Date.now()/1000;
-
-            //ROTATE TORUS
-
-            this.torus.rotation.x += 0.01;
-            this.torus.rotation.y += 0.03;
-            this.torus.rotation.z += 0.02;
-
-            this.torusS.rotation.x += 0.03;
-            this.torusS.rotation.y += 0.02;
-            this.torusS.rotation.z += 0.01;
-
-            this.torusM.rotation.x += 0.02;
-            this.torusM.rotation.y -= 0.02;
-            this.torusM.rotation.z += 0.03;
 
         //RENDER
     	this.renderer.render( this.scene, this.camera ); //Default
